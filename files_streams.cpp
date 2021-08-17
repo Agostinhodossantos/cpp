584d73d81a22fac62183379634d1fac7512f1d83
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-08-17 21:26:40 +0200

    add files_streams

---
+++ files_streams.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main () {
+    char data[100];
+
+    ofstream outfile;
+    outfile.open("afile.dat");
+
+    cout << "Writing to the file" << endl;
+    cout << "Enter your name: ";
+    cin.getline(data, 100);
+
+    // write inputted into the file
+    outfile << data << endl;
+
+    cout << "Enter your age: ";
+    cin >> data;
+    cin.ignore();
+
+    // again write inputted data into the file
+    outfile << data << endl;
+
+    // close the opened file
+    outfile.close();
+    // open a file in read mode
+    
+}
